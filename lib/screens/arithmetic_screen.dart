8687bd0473d0d818d5226713d0350ab4cbdef194
asrim10 <asrimmsuwall@gmail.com>
2025-12-02 09:49:34 +0545

    Common Snakbar made widget customized button and textfield

---
+++ lib/screens/arithmetic_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArithmeticScreen extends StatelessWidget{
+  const ArithmeticScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
